20c5aa6dc12466f7738e75795a7674860fe01d87
Carryn04 <matteo.carolla@liceobanfi.eu>
2022-04-02 15:55:39 +0200

    bellissimo

--- login.php
+++ login.php
@@ -21,7 +21,6 @@
     <div class="contenitore_login">
         <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
         <h1>LOGIN</h1>
-        <p></p>
         <h2>Inserisci le tue credenziali:</h2>
             <table>
                 <tr>
