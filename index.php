6ed37006fec29470c1069a4c9d55737b822ab2a9
RoglaPrimo <75306502+RoglaPrimo@users.noreply.github.com>
2022-04-14 17:27:52 +0200

    Andato avanti

--- index.php
+++ index.php
@@ -19,13 +19,27 @@
     <div class="header">
         <ul class="header__menu">
             <li><a href="index.php">Chi siamo</a></li>
-            <li><a href="miele.php">I nostri prodotti</a></li>
+            <li><a href="pagine.miele.php">I nostri prodotti</a></li>
             <li class="header__img">
-                <a href="index.php"><img src="../immagini/logo-removebg-preview.png" alt="Problemi nella visualizzazione del logo"></a>
+                <a href="index.php"><img src="immagini/logo-removebg-preview.png" alt="Problemi nella visualizzazione del logo"></a>
             </li>
-            <li><a href="registrazione.php">Registrati</a></li>
-            <li><a href="login.php">Login</a> </li>
+            <li><a href="pagine.registrazione.php">Registrati</a></li>
+            <li><a href="pagine.login.php">Login</a> </li>
         </ul>
+    </div>
+
+    <div class="container__Intro">
+        <div class="container__Intro__text reveal">
+
+            <p class="small-text">
+                Giro d'Italia 2021, centoquattresima edizione della corsa rosa, la più bella del mondo. Istituito nel 1909 su idea dei giornalisti della "Gazzetta dello Sport" Tullo Morgagni, Eugenio Camillo Costamagna e Armando Cougnet (esattamente come accadde appena sei anni prima con il Tour de France finanziato da "L'Auto"), è una delle tre corse a tappe di tre settimane più importanti del mondo, insieme al Tour de France e alla Vuelta a España. 
+                Storicamente è da ritenersi la seconda corsa a tappe più prestigiosa e importante del pianeta dopo quella francese, anche se, a cavallo tra gli anni quaranta e gli anni cinquanta (gli anni di Coppi e Bartali) e durante gli anni settanta (rivalità Merckx-Gimondi), il prestigio e il numero di grandi ciclisti partecipanti portarono il Giro ad avere un'importanza pari a quella del Tour.
+            </p>
+            <h2 class="big-text">The toughest race in the world's most beautiful place</h2>
+        </div>
+        <video autoplay muted loop id="video-back">
+            <source src="./immagini/Giro_Italia.mp4" type="video/mp4">
+        </video>
     </div>
 
 </body>
